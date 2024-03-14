6e529e9c14b64d765bf18f65f07645f9999b2642
Inzamam-Ul Haque <132208469+InzamamulHaque0786@users.noreply.github.com>
2024-03-14 14:24:57 +0530

    home components markup complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 import './Home.css'
 const Home = () => {
   return (
-    <div className='home-container'>Home and tijodf dfjdf</div>
+    <div className='home-container  calculator-body'>
+        <div className="home-mode">
+            mode
+        </div>
+        <div className="home-intro">
+            <p>Welcome</p>
+            <p>BMI Calculator</p>
+        </div>
+        <div className="male-female-btn">
+            <div className="male-btn">
+                Male
+            </div>
+            <div className="female-btn">
+                Female
+            </div>
+        </div>
+        <div className='home-btns'>
+            <div className="home-btns-heights">
+                height
+            </div>
+            <div className="home-btns-right">
+                <div className="home-btn-weight">
+                    Weight
+                </div>
+                <div className="home-btn-age">
+                    Age
+                </div>
+            </div>
+        </div>
+        <div className="home-result-btn">
+            Lets's go
+        </div>
+    </div>
   )
 }
 
